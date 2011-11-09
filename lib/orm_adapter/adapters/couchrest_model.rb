484dd46ef814ded0023785fdbf44cbc2784c6bb9
Leon Du <ld@ninjaconcept.com>
2011-11-09 15:40:30 +0800

    fix: should send message to klass

--- lib/orm_adapter/adapters/couchrest_model.rb
+++ lib/orm_adapter/adapters/couchrest_model.rb
@@ -38,9 +38,9 @@ module CouchRest
         # Find the first instance matching conditions
         def find_first(conditions)
           if conditions.keys.first == :id
-            get(conditions.values.first)
+            klass.get(conditions.values.first)
           else
-            send("by_#{conditions.keys.first}", {:key => conditions.values.first, :limit => 1})
+            klass.send("by_#{conditions.keys.first}", {:key => conditions.values.first, :limit => 1}).first
           end
         end
         
